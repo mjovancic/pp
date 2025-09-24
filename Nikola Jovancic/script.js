26723e0ba98ebeee14d0163a4b0e136bfa94aadc
mjovancic <milosjovancic77@gmail.com>
2025-09-24 14:18:37 +0200

    Add files via upload

---
+++ Nikola Jovancic/script.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    menu.classList.toggle('show');
+}
+
+
+// Funkcija za preklapanje menija i animaciju hamburger ikone
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const hamburger = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('show');
+    hamburger.classList.toggle('open');
+}
+
+
+
+
+
+
+
